4a7209f9ff6640a03ac5ad0c9b5fc398435c9d7e
Catherine <catherineep4189@gmail.com>
2020-05-08 13:34:51 +0100

    filtering example 2

--- ARRAYS/notes.js
+++ ARRAYS/notes.js
@@ -29,10 +29,29 @@ const notes = [{
 
 
 const filteredNotes = notes.filter(function (note, index) {
-    const isTitleMatch = note.title.toLowerCase().includes('ne')
-    const isBodyMatch = note.body.toLowerCase().includes('ne')
-    return isTitleMatch || isBodyMatch
+   const isTitleMatch = note.title.toLowerCase().includes('ne')
+   const isBodyMatch = note.body.toLowerCase().includes('ne')
+   return isTitleMatch || isBodyMatch
 
 })
 
+// filter EXAMPLE 2 
+
+// psuedocode: this example uses the same methods as the previous, however rather than hard 
+// code characters to find any matches when filtering, the argument query is passed down within
+// the filter callback method to find any characters matching to array values 
+
+const findNotes = function (notes, query) {
+   const filteredNotes = notes.filter(function (note, index) {
+      const isTitleMatch = note.title.toLowerCase().includes(query.toLowerCase())
+      const isBodyMatch = note.body.toLowerCase().includes(query.toLowerCase())
+      return isTitleMatch || isBodyMatch
+
+   })
+   return filteredNotes
+}
+
 console.log(filteredNotes)
+
+console.log(findNotes(notes, 'ci'))
+
--- ARRAYS/searching-array.js
+++ ARRAYS/searching-array.js
@@ -7,7 +7,7 @@ const notes = ['Note 1', 'Note 2', 'Note 3']
 // the indexOf method returns the first index of an element in the array
 // otherwise -1 is returned if element undefined in array
 
-// console.log(notes.indexOf('Note 2')) // returns 1
-// console.log(notes.indexOf('note 2')) // returns -1 owing to case sensitivity
+console.log(notes.indexOf('Note 2')) // returns 1
+console.log(notes.indexOf('note 2')) // returns -1 owing to case sensitivity
 
 
